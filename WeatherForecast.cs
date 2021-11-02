aa92e845a6ca90556068f5c2ce3aa6aae8a58f03
Amir Sharapov <amir@amirfounder.com>
2021-11-02 18:48:28 -0500

    initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace amir_apparel_demo_api_dotnet_5
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
